a9721b634cfc54e67d8a71c8714185c565c5df1d
Behnder <mercado.nicolas@live.com>
2022-02-15 12:04:39 -0300

    Solis SRP

---
+++ SOLID-SRP_incorrecto/Jugador.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_SRP_incorrecto
+{
+    class Jugador
+    {
+
+
+        public string Nombre { get; set; }
+        public string Clase { get; set; }
+        public float Fuerza { get; set; }
+        public float Salud { get; set; }
+        public Jugador(string nombre, string clase, float fuerza, float salud)
+        {
+            Nombre = nombre;
+            Clase = clase;
+            Fuerza = fuerza;
+            Salud = salud;
+        }
+
+        public void Atacar()
+        {
+            Console.WriteLine("Atacando con " + Fuerza + " De fuerza");
+            Console.WriteLine("...Guardando Arma");
+        }
+        public void Curar()
+        {
+            Console.WriteLine("Estado de salud = " + Salud);
+            Console.WriteLine("Curando...");
+            Salud += 1;
+            Console.WriteLine("Estado de salud = " + Salud);
+        }
+        public void CargarModelo3D()
+        {
+            Console.WriteLine("Cargando modelo del jugador... " + Nombre);
+        }
+        public void Animar()
+        {
+            Console.WriteLine("INGRESE 1 para cargar animación básica\n2 Para animación completa");
+            Console.ReadLine();
+            Console.WriteLine("Animando a " + Nombre);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SOLID-SRP_incorrecto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_SRP_incorrecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===================");
+            Console.WriteLine("NO SRP");
+            Jugador jugador = new Jugador("tony", "Ladron", 10, 50);
+            jugador.Atacar();
+            jugador.Curar();
+            jugador.CargarModelo3D();
+            jugador.Animar();
+        }
+    }
+}
